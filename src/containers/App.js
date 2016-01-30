56b2729332fb97b6146ab693001e4d5acae5faca
paulphillips <f@f.com>
2016-01-30 07:18:51 -0600

    Remove css since I'm going to do everything inline in react. Remove some boilerplate comments yeoman left.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,3 @@
-/* CAUTION: When using the generators, this file is modified in some places.
- *          This is done via AST traversal - Some of your formatting may be lost
- *          in the process - no functionality should be broken though.
- *          This modifications only run once when the generator is invoked - if
- *          you edit them, they are not updated again.
- */
 import React, {
   Component,
   PropTypes
